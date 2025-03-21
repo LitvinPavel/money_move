9bdd11d220c26779fe196291c974075d75d252f5
litvinPV <litvinpv@pba.su>
2025-03-22 01:20:40 +0300

    add db vercel

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,11 +4,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT || '5432')
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASSWORD,
+  // port: parseInt(process.env.DB_PORT || '5432')
+  connectionString: "postgres://neondb_owner:npg_tKQVZYR4r7wA@ep-shiny-dream-a5s2n8nj-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
