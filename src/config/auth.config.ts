7cf28c3966fe802ce79f5d597796c4676c86828f
litvinPV <litvinpv@pba.su>
2025-03-26 17:06:11 +0300

    fix

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -10,7 +10,7 @@ export const authConfig = {
   cookie: {
     httpOnly: true,
     secure: true, // process.env.NODE_ENV === "production"
-    sameSite: "lax" as const,
+    sameSite: "none" as const,
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 дней
   },
 };
